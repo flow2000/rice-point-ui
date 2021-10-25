2aadb8716b7e35ee4f626aabaf3ea328fddf8a23
阿海 <oncwnuP9j-MqdFla7hxbpaC9K6NU@git.weixin.qq.com>
2021-10-25 21:54:11 +0800

    菜品UI界面优化

--- src/views/dish/dish/index.vue
+++ src/views/dish/dish/index.vue
@@ -1,140 +1,170 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="菜品名称" prop="dishesName">
-        <el-input
-          v-model="queryParams.dishesName"
-          placeholder="请输入菜品名称"
-          clearable
-          size="small"
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="菜品类型" prop="dishesType">
-        <el-select v-model="queryParams.dishesType" placeholder="请选择菜品类型" clearable size="small">
-          <el-option
-            v-for="type in dishesType"
-            :key="parseInt(type.typeId)"
-            :value="type.typeName">
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="菜品价格" prop="price">
-        <el-input
-          v-model="queryParams.price"
-          placeholder="请输入菜品价格"
-          clearable
-          size="small"
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
-      </el-form-item>
-    </el-form>
+    <el-row :gutter="20">
+      <!--食堂数据-->
+      <el-col :span="4" :xs="24">
+        <div class="head-container">
+          <el-input
+            v-model="canteenName"
+            placeholder="请输入食堂名称"
+            clearable
+            size="small"
+            prefix-icon="el-icon-search"
+            style="margin-bottom: 20px"
+          />
+        </div>
+        <div class="head-container">
+          <el-tree
+            :data="canteenOptions"
+            :props="defaultProps"
+            :expand-on-click-node="false"
+            :filter-node-method="filterNode"
+            ref="tree"
+            default-expand-all
+            @node-click="handleNodeClick"
+          />
+        </div>
+      </el-col>
+      <!--用户数据-->
+      <el-col :span="20" :xs="24">
+        <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch" label-width="68px">
+          <el-form-item label="菜品名称" prop="dishesName">
+            <el-input
+              v-model="queryParams.dishesName"
+              placeholder="请输入菜品名称"
+              clearable
+              size="small"
+              @keyup.enter.native="handleQuery"
+            />
+          </el-form-item>
+          <el-form-item label="菜品类型" prop="dishesType">
+            <el-select v-model="queryParams.dishesType" placeholder="请选择菜品类型" clearable size="small">
+              <el-option
+                v-for="type in dishesType"
+                :key="parseInt(type.typeId)"
+                :value="type.typeName"
+                :disabled="type.status === '1'">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="菜品价格" prop="price">
+            <el-input
+              v-model="queryParams.price"
+              placeholder="请输入菜品价格"
+              clearable
+              size="small"
+              @keyup.enter.native="handleQuery"
+            />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+            <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+          </el-form-item>
+        </el-form>
 
-    <el-row :gutter="10" class="mb8">
-      <el-col :span="1.5">
-        <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          v-hasPermi="['dish:dish:add']"
-        >新增</el-button>
+        <el-row :gutter="10" class="mb8">
+          <el-col :span="1.5">
+            <el-button
+              type="primary"
+              plain
+              icon="el-icon-plus"
+              size="mini"
+              @click="handleAdd"
+              v-hasPermi="['dish:dish:add']"
+            >新增</el-button>
+          </el-col>
+          <el-col :span="1.5">
+            <el-button
+              type="success"
+              plain
+              icon="el-icon-edit"
+              size="mini"
+              :disabled="single"
+              @click="handleUpdate"
+              v-hasPermi="['dish:dish:edit']"
+            >修改</el-button>
+          </el-col>
+          <el-col :span="1.5">
+            <el-button
+              type="danger"
+              plain
+              icon="el-icon-delete"
+              size="mini"
+              :disabled="multiple"
+              @click="handleDelete"
+              v-hasPermi="['dish:dish:remove']"
+            >删除</el-button>
+          </el-col>
+          <el-col :span="1.5">
+            <el-button
+              type="warning"
+              plain
+              icon="el-icon-download"
+              size="mini"
+              :loading="exportLoading"
+              @click="handleExport"
+              v-hasPermi="['dish:dish:export']"
+            >导出</el-button>
+          </el-col>
+          <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+        </el-row>
+
+        <el-table v-loading="loading" :data="dishList" @selection-change="handleSelectionChange">
+          <el-table-column type="selection" width="55" align="center" />
+          <el-table-column label="菜品名称" align="center" prop="dishesName" :show-overflow-tooltip="true" />
+          <el-table-column label="菜品类型" align="center" prop="dishesType" />
+          <el-table-column label="菜品图片" align="center" prop="url" >
+            <template slot-scope="scope">
+              <el-image :src="getImage(scope.row.url)" ></el-image>
+            </template>
+          </el-table-column>
+          <el-table-column label="菜品价格" align="center" prop="price" >
+            <template slot-scope="scope">
+              <span v-if="scope.row.price!==null">￥{{scope.row.price}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="月售量" align="center" prop="onsale" />
+          <el-table-column label="菜品状态" align="center" prop="status">
+            <template slot-scope="scope">
+              <el-switch
+                v-if="scope.row.status"
+                v-model="scope.row.status"
+                active-value="0"
+                inactive-value="1"
+                @change="handleStatusChange(scope.row)"
+              ></el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                type="text"
+                icon="el-icon-edit"
+                @click="handleUpdate(scope.row)"
+                v-hasPermi="['dish:dish:edit']"
+              >修改</el-button>
+              <el-button
+                size="mini"
+                type="text"
+                icon="el-icon-delete"
+                @click="handleDelete(scope.row)"
+                v-hasPermi="['dish:dish:remove']"
+              >删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <pagination
+          v-show="total>0"
+          :total="total"
+          :page.sync="queryParams.pageNum"
+          :limit.sync="queryParams.pageSize"
+          @pagination="getList"
+        />
+
       </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['dish:dish:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['dish:dish:remove']"
-        >删除</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="el-icon-download"
-          size="mini"
-          :loading="exportLoading"
-          @click="handleExport"
-          v-hasPermi="['dish:dish:export']"
-        >导出</el-button>
-      </el-col>
-      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
-
-    <el-table v-loading="loading" :data="dishList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="菜品名称" align="center" prop="dishesName" :show-overflow-tooltip="true" />
-      <el-table-column label="菜品类型" align="center" prop="dishesType" />
-      <el-table-column label="菜品图片" align="center" prop="url" >
-        <template slot-scope="scope">
-          <el-image :src="getImage(scope.row.url)" ></el-image>
-        </template>
-      </el-table-column>
-      <el-table-column label="菜品价格" align="center" prop="price" >
-        <template slot-scope="scope">
-          <span v-if="scope.row.price!==null">￥{{scope.row.price}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="月售量" align="center" prop="onsale" />
-      <el-table-column label="菜品状态" align="center" prop="status">
-        <template slot-scope="scope">
-          <el-switch
-            v-if="scope.row.status"
-            v-model="scope.row.status"
-            active-value="0"
-            inactive-value="1"
-            @change="handleStatusChange(scope.row)"
-          ></el-switch>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-        <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
-            v-hasPermi="['dish:dish:edit']"
-          >修改</el-button>
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-delete"
-            @click="handleDelete(scope.row)"
-            v-hasPermi="['dish:dish:remove']"
-          >删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-
-    <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="queryParams.pageNum"
-      :limit.sync="queryParams.pageSize"
-      @pagination="getList"
-    />
-
     <!-- 添加或修改菜品对话框 -->
     <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
@@ -147,8 +177,20 @@
               v-for="type in dishesType"
               :key="parseInt(type.typeId)"
               :label="type.typeName"
-              :value="type.typeName">
+              :value="type.typeName"
+              :disabled="type.status === '1'">
             </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="归属食堂" prop="canteenId">
+          <el-select v-model="form.canteenIds" multiple placeholder="请选择">
+            <el-option
+              v-for="item in canteenOptions"
+              :key="item.id"
+              :label="item.label"
+              :value="item.id"
+              :disabled="item.disabled"
+            ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="菜品图片" prop="url">
@@ -178,10 +220,15 @@
 <script>
 import { listDish, getDish, delDish, addDish, updateDish, exportDish, changeDishStatus } from "@/api/dish/dish";
 import { listType } from "@/api/dish/type";
+import { listInfo } from '@/api/canteen/info'
+import { treeselect } from  "@/api/canteen/info"
+import Treeselect from '@riophae/vue-treeselect'
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
 
 export default {
   name: "Dish",
   dicts: ['sys_status'],
+  components: { Treeselect },
   data() {
     return {
       // 遮罩层
@@ -206,6 +253,12 @@ export default {
       dishesType: [],
       // 菜品类型数量
       typeTotal: 0,
+      // 食堂数据
+      canteenOptions: [],
+      // 食堂数量
+      canteenTotal: 0,
+      // 食堂名称
+      canteenName: undefined,
       // 弹出层标题
       title: "",
       // 是否显示弹出层
@@ -214,12 +267,18 @@ export default {
       queryParams: {
         pageNum: 1,
         pageSize: 10,
+        canteenId: null,
         dishesName: null,
         dishesType: null,
         price: null,
       },
       // 表单参数
       form: {},
+      defaultProps: {
+        children: "children",
+        label: "label",
+        disabled: "disabled"
+      },
       // 表单校验
       rules: {
 
@@ -232,6 +291,8 @@ export default {
       this.statusOptions = response.data
     })
     this.getType();
+    this.getCanteen();
+    this.getCanteenIds();
   },
   methods: {
     /** 查询菜品列表 */
@@ -250,6 +311,20 @@ export default {
         this.typeTotal = response.total;
       });
     },
+    /** 查询食堂下拉列表 */
+    getCanteen(){
+      treeselect().then(response => {
+        this.canteenOptions = response.data;
+        this.canteenTotal = response.total;
+      });
+    },
+    /** 查询食堂id组 */
+    getCanteenIds(){
+      treeselect().then(response => {
+        this.canteenIds = response.data;
+        this.canteenTotal = response.total;
+      });
+    },
     /** 返回图片地址 */
     getImage(url) {
       var location = window.location
@@ -266,6 +341,8 @@ export default {
         dishId: null,
         dishesName: null,
         dishesType: null,
+        canteenIds: null,
+        canteenOptions: null,
         url: null,
         price: null,
         onsale: null,
@@ -365,6 +442,16 @@ export default {
       }).catch(function() {
         row.status = row.status === '0' ? '1' : '0'
       })
+    },
+    // 筛选节点
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.label.indexOf(value) !== -1;
+    },
+    // 节点单击事件
+    handleNodeClick(data) {
+      this.queryParams.canteenId = data.id;
+      this.getList();
     },
   }
 };
